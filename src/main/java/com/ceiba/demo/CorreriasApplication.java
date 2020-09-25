9ce2a72dca64d3e45f9993429000c826bc2554ff
Giovanny Gómez - Ceiba Software <giovanny.gomez@ceiba.com.co>
2020-09-25 12:20:30 -0500

    first commit

---
+++ src/main/java/com/ceiba/demo/CorreriasApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreriasApplication.class, args);
+	}
+
+}
